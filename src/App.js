896d8dd62379979a3827be6f64dfc7ecc470a4b7
ChaonengQuan <chaonengquan@email.arizona.edu>
2021-05-01 16:40:57 -0700

    Updates: React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import resume from "./ChaonengQuan_Resume.pdf";
+import { Divider, Grid, Paper, Typography, Link } from "@material-ui/core";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                <Paper style={{ padding: 25, borderRadius: 25 }}>
+                    <Typography variant="h5">
+                        Welcome to Chaoneng Quan's Github Page
+                    </Typography>
+                    <Divider />
+                    <Typography
+                        variant="subtitle1"
+                        style={{ color: "lightgrey" }}
+                    >
+                        (this page is under construction)
+                    </Typography>
+                    <Divider />
+
+                    <Typography variant="body1">
+                        My Linkedin:
+                        <Link href="https://www.linkedin.com/in/chaonengquan">
+                            linkedin.com/in/chaonengquan
+                        </Link>
+                    </Typography>
+                    <Typography variant="body1">
+                        My Github:
+                        <Link href="https://github.com/ChaonengQuan">
+                            github.com/ChaonengQuan
+                        </Link>
+                    </Typography>
+
+                    <Typography variant="body1">My Resume:</Typography>
+                    <Document file={resume}>
+                        <Page pageNumber={1} />
+                    </Document>
+                </Paper>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
